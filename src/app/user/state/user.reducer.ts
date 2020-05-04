5b614d1def792e90e9a2ef68a73b07db5e892a1a
Faris bin Abdul hameed <faris.hameed@uniquesoftwaredev.com>
2020-05-04 01:27:09 -0500

    initial commit

---
+++ src/app/user/state/user.reducer.ts
@@ -0,0 +1,16 @@
+
+export interface UserState {
+  maskUserName : boolean
+}
+
+export function reducer(state = {}, action) {
+  switch (action.type) {
+    case "MASK_USER_NAME":
+      return {
+        ...state,
+        maskUserName: action.payload,
+      };
+    default:
+      return state;
+  }
+}
